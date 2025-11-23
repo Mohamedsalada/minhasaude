8d37a69b9349f73e3abd4f7b271370774a59d512
Falafino <joaodiasmoraes51@gmail.com>
2025-11-23 20:01:29 -0300

    plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
+  providers: [ScreenOrientation],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private screenOrientation: ScreenOrientation) {
+    
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+  }
 }
+
